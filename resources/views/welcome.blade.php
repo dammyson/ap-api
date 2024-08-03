868d875f7cb226beefa05ffbdaba421b816ab87f
Ayobami Ayeni <ayobami@Ayobamis-MacBook-Pro-2.local>
2024-08-03 11:25:52 -0500

    Test Main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>AIR PEACE</title>
+        <title>AIR PEACE API</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
