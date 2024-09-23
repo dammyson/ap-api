22dcf9cd2001bb87598a523f0b3e8ed1a9bdc50c
Emeka-techDev <gilbertgenye@gmail.com>
2024-09-23 12:28:04 +0100

    list of available flights

--- app/Http/Controllers/Test/GetAirportMatrixController.php
+++ app/Http/Controllers/Test/GetAirportMatrixController.php
@@ -9,15 +9,87 @@ use Illuminate\Http\Request;
 class GetAirportMatrixController extends Controller
 {
     protected $getAirportBuilder;
+    protected $craneOTASoapService;
+       
 
     public function __construct(GetAirportMatrixBuilder $getAirportBuilder)
     {
         $this->getAirportBuilder = $getAirportBuilder;
+        $this->craneOTASoapService = app('CraneOTASoapService');
     }
 
     public function GetAirportMatrix() {
+        $function = 'http://impl.soap.ws.crane.hititcs.com/GetAirPortMatrix';
         $xml = $this->getAirportBuilder->GetAirportMatrix();
 
-        dd($xml);
+        try {
+            $response = $this->craneOTASoapService->run($function, $xml);
+            $airportMatrixList = $response['AirPortMatrixResponse']['airPortMatrixInfo']['airPortMatrixList'];
+            $availableFlights = [];
+            
+            foreach($airportMatrixList as $airportMatrix) {
+                $originLocationCityCode =  $airportMatrix['origin']['city']['locationCode'];
+                $originLocationCityName = $airportMatrix['origin']['city']['locationName'];
+                $originLocationCountryCode = $airportMatrix['origin']['country']['locationCode'];
+                $originLocationCountryName = $airportMatrix['origin']['country']['locationName'];
+            
+                $departureFlights = $airportMatrix['destinationList'];
+                $departureFlightArray = [];
+
+                if (array_key_exists('city', $departureFlights)) {
+                    $departureCityLocationCode = $departureFlights['city']['locationCode'];
+                    $departureCityLocationName = $departureFlights['city']['locationName'];
+                    $departureCountryLocationCode = $departureFlights['country']['locationCode'];
+                    $departureCountryLocationName = $departureFlights['country']['locationName'];
+                    
+                    $departureFlightArray[] = [
+                        "departureCityLocationCode" => $departureCityLocationCode,
+                        "departureCityLocationName" => $departureCityLocationName,
+                        "departureCountryLocationCode" => $departureCountryLocationCode,
+                        "departureCountryLocationName" => $departureCountryLocationName
+                    ];
+                } else  {
+                    foreach($departureFlights as $departureFlight) {
+                        $departureCityLocationCode = $departureFlight['city']['locationCode'];
+                        $departureCityLocationName = $departureFlight['city']['locationName'];
+                        $departureCountryLocationCode = $departureFlight['country']['locationCode'];
+                        $departureCountryLocationName = $departureFlight['country']['locationName'];
+                        
+                        $departureFlightArray[] = [
+                            "departureCityLocationCode" => $departureCityLocationCode,
+                            "departureCityLocationName" => $departureCityLocationName,
+                            "departureCountryLocationCode" => $departureCountryLocationCode,
+                            "departureCountryLocationName" => $departureCountryLocationName
+                        ];
+                    } 
+                }
+
+                $availableFlightDetails = [
+                    "originAndDestinations"  => [
+                        "originLocationCityCode" => $originLocationCityCode,
+                        "originLocationCityName" => $originLocationCityName,
+                        "originLocationCountryCode" => $originLocationCountryCode,
+                        "originLocationCountryName" => $originLocationCountryName,
+                        "destinations" => $departureFlightArray
+                    ]                    
+                ];
+
+                $availableFlights[] = $availableFlightDetails;
+            }
+        
+        } catch (\Throwable $th) {
+            return response()->json([
+                "error" => true,
+                "message" => $th->getMessage()
+            ], 500);
+
+        }
+
+        return response()->json([
+            "error" => "false",
+            "message" => "available flights",
+            "availableFlights" => $availableFlights
+        ], 200);
+
     }
 }
