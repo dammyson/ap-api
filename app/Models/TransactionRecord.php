cb3634ef0c9ddfc37b3889456634299380052dc2
Emeka-techDev <gilbertgenye@gmail.com>
2024-09-29 16:43:34 +0100

    transaction record

--- app/Models/TransactionRecord.php
+++ app/Models/TransactionRecord.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionRecord extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [ 
+        'user_name', 
+        'peace_id', 
+        'amount', 
+        'ticket_type', 
+        'ticket_number', 
+        'lead_passenger_email',
+        'payment_reference', 
+        'invoice_number', 
+        'reason_for_issuance', 
+        'order_id'
+    ];
 }
