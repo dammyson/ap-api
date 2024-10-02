ecc38994237401686ea35815b23cc179fe4472d0
Ayobami Ayeni <ayobami@Ayobamis-MacBook-Pro-2.local>
2024-10-01 21:42:06 -0500

    update to multi city

--- app/Http/Requests/Passenger/SearchFlightRequest.php
+++ app/Http/Requests/Passenger/SearchFlightRequest.php
@@ -22,11 +22,21 @@ class SearchFlightRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'departure_date' => 'required|date',
-            'arrival_date' => 'required|date',
+
             'trip_type' => 'required|string|in:ONE_WAY,ROUND_TRIP,MULTI_DIRECTIONAL',
+
+            'departure_date' => 'required_if:trip_type,ONE_WAY,ROUND_TRIP|date',
+            'arrival_date' => 'required_if:trip_type,ROUND_TRIP|date|after:departure_date',
+           
             'departure_airport' => 'required|string',
             'arrival_airport' => 'required|string',
+
+            // Validation for MULTI_DIRECTIONAL
+            'multi_directional_flights' => 'required_if:trip_type,MULTI_DIRECTIONAL|array',
+            'multi_directional_flights.*.departure_airport' => 'required_if:trip_type,MULTI_DIRECTIONAL|string|size:3',
+            'multi_directional_flights.*.departure_date' => 'required_if:trip_type,MULTI_DIRECTIONAL|date',
+            'multi_directional_flights.*.arrival_airport' => 'required_if:trip_type,MULTI_DIRECTIONAL|string|size:3',
+
             'travelerInformation' => 'required|array',
             'travelerInformation.*.passenger_type' => 'required|string',
             'travelerInformation.*.passengers' => 'required|integer|min:1',
