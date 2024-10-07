2e6bca13bec921dd84a453dee3d3a2e6794808e3
Emeka-techDev <gilbertgenye@gmail.com>
2024-10-07 15:48:47 +0100

    updated

---
+++ app/Http/Requests/ChangePeaceIdRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePeaceIdRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "peace_id" => "required|string|min:11|max:11"
+        ];
+    }
+}
