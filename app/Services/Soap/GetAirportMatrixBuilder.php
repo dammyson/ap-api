6735b4549e53aa3be1da487690da2cf69e88c804
Emeka-techDev <gilbertgenye@gmail.com>
2024-11-08 14:11:18 +0100

    search flight fixed

--- app/Services/Soap/GetAirportMatrixBuilder.php
+++ app/Services/Soap/GetAirportMatrixBuilder.php
@@ -5,20 +5,21 @@ namespace App\Services\Soap;
 class GetAirportMatrixBuilder {
     
     public function GetAirportMatrix() {
-        $xml ='<?xml version="1.0" encoding="UTF-8"?> 
-         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:impl="http://impl.soap.ws.crane.hititcs.com/">
-         <soapenv:Header/>
+        $xml ='
+        <?xml version="1.0" encoding="UTF-8"?> 
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:impl="http://impl.soap.ws.crane.hititcs.com/">
+            <soapenv:Header/>
             <soapenv:Body>
-               <impl:GetAirPortMatrix>
-               <AirPortMatrixRequest>
-               <clientInformation>
-               <clientIP>129.0.0.1</clientIP>
-               <member>false</member>
-               <password>SCINTILLA</password>
-               <userName>SCINTILLA</userName>
-               </clientInformation>
-               </AirPortMatrixRequest>
-               </impl:GetAirPortMatrix>
+                <impl:GetAirPortMatrix>
+                    <AirPortMatrixRequest>
+                    <clientInformation>
+                    <clientIP>129.0.0.1</clientIP>
+                    <member>false</member>
+                    <password>SCINTILLA</password>
+                    <userName>SCINTILLA</userName>
+                    </clientInformation>
+                    </AirPortMatrixRequest>
+                </impl:GetAirPortMatrix>
             </soapenv:Body>
         </soapenv:Envelope>';
 
