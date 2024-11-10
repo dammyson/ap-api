50518daa229d4a3250426059d08bc5ae603902ee
Emeka-techDev <gilbertgenye@gmail.com>
2024-11-10 11:27:32 +0100

    is published returned

--- app/Models/Admin/Survey.php
+++ app/Models/Admin/Survey.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'duration_of_survey', 'image_url', 'points_awarded', 'is_active'];
+    protected $fillable = ['title', 'duration_of_survey', 'image_url', 'points_awarded', 'is_active', 'is_published'];
 
     public function questions() {
         return $this->hasMany(Question::class);
