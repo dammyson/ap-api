d83c0f02329738c074decd4957f4dd5a5883a82f
Emeka-techDev <gilbertgenye@gmail.com>
2024-12-04 13:01:14 +0100

    upcoming trips

--- routes/api.php
+++ routes/api.php
@@ -301,6 +301,7 @@ Route::group(["middleware" => ["auth:api"]], function () {
         Route::post('user-logout', [RegisterController::class, 'logoutUser']);
         Route::post('create-wallet', [WalletController::class, 'createWallet']);
         Route::get('trip-history', [AnalyticsUserController::class, 'tripHistory']);
+        Route::get('upcoming-trips', [AnalyticsUserController::class, 'upcomingTrips']);
     });
 
 
