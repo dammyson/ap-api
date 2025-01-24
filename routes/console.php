a16531cdb7b215b0e6e420250729e10879b8805d
Emeka-techDev <gilbertgenye@gmail.com>
2025-01-24 11:23:58 +0100

    deactive survey

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Models\Admin\Survey;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::call(function (Illuminate\Console\Scheduling\Schedule $schedule) {
+    $schedule->call(function () {
+        $expiredSurveys = Survey::where('is_active', true)
+            ->where('is_published', true)
+            ->whereRaw("DATE_ADD(updated_at, INTERVAL duration_of_survey MINUTE) <= NOW()")
+            ->first();
+
+        $expiredSurveys->is_active = false;
+        $expiredSurveys->save();
+
+        // foreach ($expiredSurveys as $survey) {
+        //     $survey->update(['is_active' => false]);
+        //     logger()->info("Deactivated Survey ID: {$survey->id}");
+        // }
+    })->hourly(); // Adjust the frequency as needed
+});
