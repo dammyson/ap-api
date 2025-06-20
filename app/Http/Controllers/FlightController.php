2a5dc09d669d167f61e85252472d0301b9c04972
Emeka-techDev <gilbertgenye@gmail.com>
2025-06-20 13:36:27 +0100

    search flight message

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -83,7 +83,7 @@ class FlightController extends Controller
                 if(!array_key_exists('originDestinationOptionList', $availabilityByDateList)) {
                     return response()->json([
                         'error' => true,
-                        'message' => "this flight is short of neccessary data"
+                        'message' => "no availability"
                     ], 500);
                 }
 
@@ -97,7 +97,7 @@ class FlightController extends Controller
                 if(!array_key_exists('originDestinationOptionList', $availabilityByDateList)) {
                     return response()->json([
                         'error' => true,
-                        'message' => "flight is short of neccessary data"
+                        'message' => "no availability"
                     ], 500);
                 }
 
@@ -105,7 +105,7 @@ class FlightController extends Controller
                 if(!$originDestinationOptionList0) {
                     return response()->json([
                         'error' => true,
-                        'message' => "flight is short of neccessary data"
+                        'message' => "no availability"
                     ], 500);
                 }
                 $result0 = $this->groupFaresByCabin($originDestinationOptionList0, $quantity, $travelerInformation_count);
@@ -149,120 +149,7 @@ class FlightController extends Controller
             ], 500);
         }
     }
-    public function searchFlightsForWeb(SearchFlightRequest $request)
-    {
-
-        $departureDateTime = $request->input('departure_date');
-        $ArrivalDateTime = $request->input('arrival_date');
-        $destinationLocationCode = $request->input('arrival_airport');
-        $originLocationCode = $request->input('departure_airport');
-        $preferredCurrency = $request->input('preferred_currency');
-
-        $quantity = $request->input('passengers');
-        
-        $tripType = $request->input('trip_type');
-
-        $validated = $request->validated();
-
-      
-        $travelerInformation = $validated["travelerInformation"];
-        $travelerInformation_count = count($travelerInformation);
-
-      
-
-        $function = 'http://impl.soap.ws.crane.hititcs.com/GetAvailability';
-
-        if ($request->input('trip_type') == "ONE_WAY") {
-            $xml = $this->soapRequestBuilder->GetFlightOneWay($preferredCurrency, $departureDateTime, $destinationLocationCode, $originLocationCode, $travelerInformation, $tripType);
-            // dd($xml);
-        } else  if ($request->input('trip_type') == "ROUND_TRIP") {
-            $xml = $this->soapRequestBuilder->GetFlightRoundTrip($preferredCurrency, $departureDateTime, $destinationLocationCode, $originLocationCode, $travelerInformation, $tripType,  $ArrivalDateTime);
-        } else {
-            $multiDirectionalFlights = $validated['multi_directional_flights'];
-
-            $xml = $this->soapRequestBuilder->GetFlightMultiCity($preferredCurrency, $multiDirectionalFlights, $travelerInformation, $tripType);
-        }
-        // dd($xml);
-        try {
-
-            $response = $this->craneOTASoapService->run($function, $xml);
-            
-            // dd($response);
-            // return $response;
-
-            $result = "";
-
-            if ($request->input('trip_type') == "ONE_WAY") {
-                $availabilityByDateList = $response['Availability']['availabilityResultList']['availabilityRouteList']['availabilityByDateList'];
-                if(!array_key_exists('originDestinationOptionList', $availabilityByDateList)) {
-                    return response()->json([
-                        'error' => true,
-                        'message' => "this flight is short of neccessary data"
-                    ], 500);
-                }
-
-                $originDestinationOptionList = $response['Availability']['availabilityResultList']['availabilityRouteList']['availabilityByDateList']['originDestinationOptionList'];
-                $result0 = $this->groupFaresByCabin($originDestinationOptionList, $quantity, $travelerInformation_count);
-                $rt = new \stdClass();
-                $rt->departure = $result0;
-                $result = $rt;
-            } else  if ($request->input('trip_type') == "ROUND_TRIP") {
-                $availabilityByDateList = $response['Availability']['availabilityResultList']['availabilityRouteList'][0]['availabilityByDateList'];
-                if(!array_key_exists('originDestinationOptionList', $availabilityByDateList)) {
-                    return response()->json([
-                        'error' => true,
-                        'message' => "flight is short of neccessary data"
-                    ], 500);
-                }
-
-                $originDestinationOptionList0 = $response['Availability']['availabilityResultList']['availabilityRouteList'][0]['availabilityByDateList']['originDestinationOptionList'];
-                if(!$originDestinationOptionList0) {
-                    return response()->json([
-                        'error' => true,
-                        'message' => "flight is short of neccessary data"
-                    ], 500);
-                }
-                $result0 = $this->groupFaresByCabin($originDestinationOptionList0, $quantity, $travelerInformation_count);
-                $originDestinationOptionList1 = $response['Availability']['availabilityResultList']['availabilityRouteList'][1]['availabilityByDateList']['originDestinationOptionList'];
-                $result1 = $this->groupFaresByCabin($originDestinationOptionList1, $quantity,  $travelerInformation_count);
-
-                $rt = new \stdClass();
-                $rt->departure = $result0;
-                $rt->arrival = $result1;
-                $result = $rt;
-            }else{
-                $availabilityRouteList = $response['Availability']['availabilityResultList']['availabilityRouteList'];
-
-                $multiDirectionalFlights = $validated['multi_directional_flights'];
-
-                $rt = new \stdClass();
-
-                for ($i = 0; $i < count( $availabilityRouteList); $i++) {
-
-                    $mainkey =  $multiDirectionalFlights[$i]['departure_airport'] . " - " .  $multiDirectionalFlights[$i]['arrival_airport'];
-
-                    $originDestinationOptionList =  $availabilityRouteList[$i];
-                  
-                    $result = $this->groupFaresByCabin($originDestinationOptionList['availabilityByDateList']['originDestinationOptionList'], $quantity, $travelerInformation_count);
-                    $rt->{$mainkey}  =  $result;
-                }
-               // $originDestinationOptionList0 = $response['Availability']['availabilityResultList']['availabilityRouteList'][0]['availabilityByDateList']['originDestinationOptionList'];
-               // dd(  $availabilityRouteList);
-
-               $result = $rt;
-
-            }
-           
-            return response()->json($result);
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-
-            return response()->json([
-                "error" => true,            
-                "message" => "something went wrong"
-            ], 500);
-        }
-    }
+    
 
 
     public function groupFaresByCabin($originDestinationOptionList, $quantity,  $count)
