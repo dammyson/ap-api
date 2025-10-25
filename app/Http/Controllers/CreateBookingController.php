1f92542a8e60ba9a59feebfc9efa8dd688bca997
Ayo Ayeni <ayo.ayeni@siu.edu>
2025-10-25 17:50:42 -0500

    update

--- app/Http/Controllers/CreateBookingController.php
+++ app/Http/Controllers/CreateBookingController.php
@@ -64,7 +64,7 @@ class CreateBookingController extends Controller
             $specialServiceRequestList
         );
 
-        dd($xml);
+      //  dd($xml);
 
         $function = 'http://impl.soap.ws.crane.hititcs.com/CreateBooking';
         try {
