68c1e17b4a2b9d48ec75a304dfd36de9732c471b
Emeka-techDev <gilbertgenye@gmail.com>
2026-02-12 11:02:43 +0100

    ticket reservation controller

--- app/Http/Controllers/Soap/TicketReservationController.php
+++ app/Http/Controllers/Soap/TicketReservationController.php
@@ -192,8 +192,8 @@ class TicketReservationController extends Controller
                         'invoice_id' => $invoice->id,
                         'device_type' => $deviceType,
                         'is_flight' => true,                             
-                        "payment_method" => $paymentMethod,
-                        "payment_channel" => $paymentChannel,
+                        "payment_method" => $paymentMethod ?? "not applicable",
+                        "payment_channel" => $paymentChannel ?? "not applicable",
                         'currency' => $preferredCurrency
 
                     ]);                          
