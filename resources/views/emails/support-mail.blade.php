69b584455089c44bba4f02e644f84f9a35a0a200
Emeka-techDev <gilbertgenye@gmail.com>
2026-02-21 10:07:38 +0100

    email fix

--- resources/views/emails/support-mail.blade.php
+++ resources/views/emails/support-mail.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
 </head>
 <body style="font-family: Arial, sans-serif;">
+    <img
+        src="{{ public_path('images/airpeace-logo.png') }}" 
+        style="width:100%; max-height:300px; display:block; margin:0 auto 10px auto;"
+    />
 
     <h2>Customer complaint</p>
 
